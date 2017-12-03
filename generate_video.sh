3ff0e1636d7885b88346854b7ddaec34f249ecff
S.P. Mohanty <spmohanty91@gmail.com>
2017-12-03 08:13:42 +0100

    Add video generation scripts

---
+++ generate_video.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SUBMISSION_ID=$1
+CURRENT_DIR=`pwd`
+cd data/$SUBMISSION_ID/logs/video_frames/$SUBMISSION_ID
+echo "Converting PNGs to GIF..."
+convert -delay 5 -loop 1 -verbose *.png video.gif
+echo "Converting GIF to MP4..."
+ffmpeg -y -an -i video.gif -vcodec libx264 -pix_fmt yuv420p -profile:v baseline -level 3 $SUBMISSION_ID.mp4
+echo "Creating MP4 Thumb..."
+ffmpeg -y -i $SUBMISSION_ID.mp4 -vf scale=268:200 -c:a copy $SUBMISSION_ID.thumb.mp4
+cd $CURRENT_DIR
---
+++ generate_videos.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from multiprocessing import Pool
+import glob
+import os
+
+SUBMISSION_IDS = []
+for _video_dir in glob.glob("data/*/logs/video_frames"):
+    _sub_id = _video_dir.split("/")[-3]
+    SUBMISSION_IDS.append(_sub_id)
+
+print(SUBMISSION_IDS)
+def process(sid):
+    os.system("./generate_video.sh "+str(sid))
+
+p = Pool(30)
+p.map(process, SUBMISSION_IDS)
